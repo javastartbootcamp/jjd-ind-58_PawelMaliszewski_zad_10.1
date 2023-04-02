06f18489ca9df948c0e0d06bd4bf8ef7c9a8d2e7
Pawel Maliszewski <pablo999455@gmail.com>
2023-04-02 12:00:05 +0200

    solution v2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,26 +3,15 @@ package pl.javastart.task;
 import java.util.Scanner;
 
 public class Main {
-
     public static final int MAX_MEMORY = 3;
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
         Memory[] memories = new Memory[MAX_MEMORY];
-
         int memorycount = 0;
-
         System.out.println("Podaj informację o trzech unikalnych pamięciach RAM");
-
         while (memorycount < memories.length) {
-            System.out.println("Podaj rodzaj pamięci");
-            String name = sc.nextLine();
-            System.out.println("Podaj pojemność pamięci");
-            int size = sc.nextInt();
-            sc.nextLine();
-            Memory tempMemory = new Memory(name, size);
+            Memory tempMemory = createMemory();
             if (checkIfExists(tempMemory, memories)) {
                 System.out.println("Duplikat");
             } else {
@@ -30,10 +19,19 @@ public class Main {
                 memorycount++;
             }
         }
-
         for (Memory memory : memories) {
             System.out.println(memory);
         }
+    }
+
+    private static Memory createMemory() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj rodzaj pamięci");
+        String name = sc.nextLine();
+        System.out.println("Podaj pojemność pamięci");
+        int size = sc.nextInt();
+        sc.nextLine();
+        return new Memory(name, size);
     }
 
     static boolean checkIfExists(Memory tmpMemory, Memory[] memories) {
